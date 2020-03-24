4a6fa9f7ce082fe5672c994f7df377792b426031
Tope Salahudeen <salahudeentope@gmail.com>
2020-03-25 00:43:05 +0100

    changed the unit from meters to cm

--- Area_of_a_circle.py
+++ Area_of_a_circle.py
@@ -1,9 +1,9 @@
 import math
 
 
-def area_of_a_circle(radius=input('input the radius of the circle here in meters: ')):
+def area_of_a_circle(radius=input('input the radius of the circle here in cm: ')):
     area = math.pi * pow(float(radius), 2)
-    return f"Area of the circle with radius {radius}m is {round(area, 2)}m\u00b2"
+    return f"Area of the circle with radius {radius}cm is {round(area, 2)}cm\u00b2"
 
 
 if __name__ == '__main__':
