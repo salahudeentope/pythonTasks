95988bad9193b8f863beaaa4222a46e50906f37f
Tope Salahudeen <salahudeentope@gmail.com>
2020-03-25 12:43:20 +0100

    Added codes for exception handling

--- Area_of_a_circle.py
+++ Area_of_a_circle.py
@@ -2,8 +2,15 @@ import math
 
 
 def area_of_a_circle(radius=input('input the radius of the circle here in cm: ')):
-    area = math.pi * pow(float(radius), 2)
-    return f"Area of the circle with radius {radius}cm is {round(area, 2)}cm\u00b2"
+    try:
+        assert float(radius) >= 0
+        area = math.pi * pow(float(radius), 2)
+    except AssertionError:
+        return 'The radius of the circle cannot be a negative number'
+    except ValueError:
+        return "The radius of the circle has to be a number not string"
+    else:
+        return f"Area of the circle with radius {radius}cm is {round(area, 2)}cm\u00b2"
 
 
 if __name__ == '__main__':
